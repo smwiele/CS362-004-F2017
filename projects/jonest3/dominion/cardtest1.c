d363294337d54c09a4acee4cd6954bd189ea5349
Taylor Jones <jonest3@flip2.engr.oregonstate.edu>
2017-10-21 20:45:18 -0700

    Started on cardtests.

--- projects/jonest3/dominion/cardtest1.c
+++ projects/jonest3/dominion/cardtest1.c
@@ -1,3 +1,19 @@
 /*jonest3
  * Card Test 1*/
 
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// adventurer card
+
+// int adventurer_func(int drawntreasure, struct gameState *state, int currentPlayer, int temphand[], int z)
+
+
+int main(){
+
+   return 0;
+}
